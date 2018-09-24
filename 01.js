4afbd1b857b5cc8b843593f25802daae74c772db
bilalmakayasa <43075125+bilalmakayasa@users.noreply.github.com>
2018-09-24 19:24:47 +0700

    Delete 01.js

--- 01.js
+++
@@ -1,9 +0,0 @@
-function isPalindrome(teks)
-{ 
-    let splitTeks = teks.split('');
-    let reverseTeksArray = splitTeks.reverse();
-    let joinTeksArray = reverseTeksArray.join('') 
-    console.log(joinTeksArray)
-}
-
-isPalindrome('test')
